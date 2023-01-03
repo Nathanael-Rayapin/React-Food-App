ce0571e230bc6502167094f5eace90f7e7ccb6e6
Nathanael421 <nathanael.rayapin@gmail.com>
2023-01-03 12:05:29 +0100

    Init Food-App Application

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+import AvailableMeals from "./AvailableMeals";
+
+import MealsSummary from './MealsSummary.js';
+
+const Meals = () => {
+    return(
+        <Fragment>
+            <MealsSummary />
+            <AvailableMeals />
+        </Fragment>
+    );
+};
+
+export default Meals;
